d621952e6cbce9fe1aba0c3edbd36bebc35f9f8f
AnatolyWassermann <99121106+AnatolyWassermann@users.noreply.github.com>
2022-03-23 21:12:47 +0300

    Add files via upload

---
+++ Day11 - Blackjack.py
@@ -0,0 +1,99 @@
+import random
+from asciis import logo_blackjack
+# a take on blackjack. only difference is dealer only pulling 2 cards,
+players_deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+dealers_deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+player = []
+dealer = []
+win = ["opponent went over. you win 😜", "it's unbelievable you won! 😁",
+       "believe it or not but you made it 😎"]
+lose = ["you fucked up pretty badly 😕",
+        "you lose i feel bad right now because of your failure 😓",
+        "unfortunately you lose 😞"]
+draw = ["eny meny miny moe where my gloves will go! it's a draw 😑",
+        "sorry but it's a draw tops lel", "DRAW!"]
+
+
+def starting_hand():
+    """game starts 2 cards for each side, but only shows dealer's first card
+    shuffling both lists to get random cards then remove them from
+    actual lists, no duplicate cards """
+    random.shuffle(players_deck)
+    random.shuffle(dealers_deck)
+    for i in range(2):
+        player.append(players_deck[0])
+        players_deck.pop(0)
+        dealer.append(dealers_deck[0])
+        dealers_deck.pop(0)
+    score()
+    blackjack()
+
+
+def score():
+    """showing the current score"""
+    print(f"    Your cards: {player}, Current score: {sum(player)}")
+    print(f"    Computer's first card: {dealer[0]}")
+
+
+def final_score():
+    """showing the final score"""
+    print(f"    Your final hand: {player}, Final Score: {sum(player)}")
+    print(f"    Computer's final hand: {dealer}, Final Score: {sum(dealer)}")
+
+
+def blackjack():
+    """ body of the game, pulling cards when needed then if sum > 21
+    call calculate_score() else recursion."""
+    console = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    if console == 'y':
+        player.append(players_deck[0])
+        players_deck.pop(0)
+        if sum(player) > 21:
+            calculate_score()
+        else:
+            score()
+            blackjack()
+    else:
+        final_score()
+        calculate_score()
+
+
+def calculate_score():
+    """calculate the score then conclude the game or call blackjack()"""
+    if 11 not in player and sum(player) > 21:
+        final_score()
+        print(random.choice(lose))
+    elif 11 in player and sum(player) > 21:
+        player.remove(11)
+        player.append(1)
+        if sum(player) > 21:
+            final_score()
+            print(random.choice(lose))
+        else:
+            score()
+            blackjack()
+    elif sum(player) == sum(dealer):
+        print(random.choice(draw))
+    elif sum(player) == 21 and len(player) == 2:
+        print("BLACKJACK BITCH!! NOW WE'RE TALKING!!!")
+    elif 21 >= sum(player) > sum(dealer):
+        print(random.choice(win))
+    elif 21 >= sum(dealer) > sum(player):
+        print(random.choice(lose))
+    else:
+        score()
+        blackjack()
+
+
+def game_start():
+    game = input(
+        "Do you want to play a game of Blackjack? type 'y' or 'n': ").lower()
+    if game == 'y':
+        print(logo_blackjack)
+        starting_hand()
+    else:
+        print("BYE BYE")
+
+
+game_start()
---
+++ asciis.py
@@ -0,0 +1,335 @@
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',
+'awkward',
+'axiom',
+'azure',
+'bagpipes',
+'bandwagon',
+'banjo',
+'bayou',
+'beekeeper',
+'bikini',
+'blitz',
+'blizzard',
+'boggle',
+'bookworm',
+'boxcar',
+'boxful',
+'buckaroo',
+'buffalo',
+'buffoon',
+'buxom',
+'buzzard',
+'buzzing',
+'buzzwords',
+'caliph',
+'cobweb',
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'transplant',
+'triphthong',
+'twelfth',
+'twelfths',
+'unknown',
+'unworthy',
+'unzip',
+'uptown',
+'vaporize',
+'vixen',
+'vodka',
+'voodoo',
+'vortex',
+'voyeurism',
+'walkway',
+'waltz',
+'wave',
+'wavy',
+'waxy',
+'wellspring',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
+
+stages = ['''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    ''']
+
+logo = '''88  88    db    88b 88  dP""b8 8b    d8    db    88b 88 
+88  88   dPYb   88Yb88 dP   `" 88b  d88   dPYb   88Yb88 
+888888  dP__Yb  88 Y88 Yb  "88 88YbdP88  dP__Yb  88 Y88 
+88  88 dP""""Yb 88  Y8  YboodP 88 YY 88 dP""""Yb 88  Y8 '''
+you_won = '''___  _ ____  _       _      ____  _     
+\  \///  _ \/ \ /\  / \  /|/  _ \/ \  /|
+ \  / | / \|| | ||  | |  ||| / \|| |\ ||
+ / /  | \_/|| \_/|  | |/\||| \_/|| | \||
+/_/   \____/\____/  \_/  \|\____/\_/  \|
+                                        '''
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z']
+logo_caesar = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+logo_auction = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+
+logo_calculator =  """.o88b.  .d8b.  db       .o88b. db    db db       .d8b.  d888888b  .d88b.  d8888b. 
+d8P  Y8 d8' `8b 88      d8P  Y8 88    88 88      d8' `8b `~~88~~' .8P  Y8. 88  `8D 
+8P      88ooo88 88      8P      88    88 88      88ooo88    88    88    88 88oobY' 
+8b      88~~~88 88      8b      88    88 88      88~~~88    88    88    88 88`8b   
+Y8b  d8 88   88 88booo. Y8b  d8 88b  d88 88booo. 88   88    88    `8b  d8' 88 `88. 
+ `Y88P' YP   YP Y88888P  `Y88P' ~Y8888P' Y88888P YP   YP    YP     `Y88P'  88   YD """
+
+logo_blackjack = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
